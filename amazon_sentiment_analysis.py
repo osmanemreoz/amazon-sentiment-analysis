d2780914649b22a2eed8f6ab4d64cc5550fe2bbe
emreoz <75579555+osmanemreoz@users.noreply.github.com>
2022-12-26 14:06:59 +0300

    Add files via upload

---
+++ amazon_sentiment_analysis.py
@@ -0,0 +1,241 @@
+from warnings import filterwarnings
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from PIL import Image
+from nltk.corpus import stopwords
+from nltk.sentiment import SentimentIntensityAnalyzer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import cross_val_score, GridSearchCV, cross_validate
+from sklearn.preprocessing import LabelEncoder
+from textblob import Word, TextBlob
+from wordcloud import WordCloud
+
+filterwarnings('ignore')
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 200)
+pd.set_option('display.float_format', lambda x: '%.2f' % x)
+
+##################################################
+# 1. Text Preprocessing
+##################################################
+
+df = pd.read_excel("Natural Language Processing (NLP)/Ödev/KOZMOS-Amazon Yorumları için Duygu Analizi/amazon.xlsx")
+df.head()
+
+###############################
+# Normalizing Case Folding
+###############################
+
+df['Review'] = df['Review'].str.lower()
+
+###############################
+# Punctuations
+###############################
+
+df['Review'] = df['Review'].str.replace('[^\w\s]', '')
+
+###############################
+# Numbers
+###############################
+
+df['Review'] = df['Review'].str.replace('\d', '')
+
+###############################
+# Stopwords
+###############################
+import nltk
+# nltk.download('stopwords')
+
+sw = stopwords.words('english')
+
+df['Review'] = df['Review'].apply(lambda x: " ".join(x for x in str(x).split() if x not in sw))
+
+###############################
+# Rarewords
+###############################
+
+temp_df = pd.Series(' '.join(df['Review']).split()).value_counts()
+
+drops = temp_df[temp_df <= 1]
+
+df['Review'] = df['Review'].apply(lambda x: " ".join(x for x in x.split() if x not in drops))
+
+###############################
+# Lemmatization
+###############################
+
+# nltk.download('wordnet')
+df['Review'] = df['Review'].apply(lambda x: " ".join([Word(word).lemmatize() for word in x.split()]))
+
+df.head()
+
+##################################################
+# 2. Text Visualization
+##################################################
+
+###############################
+# Terim Frekanslarının Hesaplanması
+###############################
+
+tf = df["Review"].apply(lambda x: pd.value_counts(x.split(" "))).sum(axis=0).reset_index()
+
+tf.columns = ["words", "tf"]
+
+tf.sort_values("tf", ascending=False)
+
+###############################
+# Barplot
+###############################
+
+tf[tf["tf"] > 500].plot.bar(x="words", y="tf")
+plt.show()
+
+###############################
+# Wordcloud
+###############################
+
+text = " ".join(i for i in df.Review)
+
+wordcloud = WordCloud().generate(text)
+plt.imshow(wordcloud, interpolation="bilinear")
+plt.axis("off")
+plt.show()
+
+wordcloud = WordCloud(max_font_size=50,
+                      max_words=100,
+                      background_color="white").generate(text)
+plt.figure()
+plt.imshow(wordcloud, interpolation="bilinear")
+plt.axis("off")
+plt.show()
+
+wordcloud.to_file("amazon.png")
+
+###############################
+# Şablonlara Göre Wordcloud
+###############################
+
+tr_mask = np.array(Image.open("Natural Language Processing (NLP)/Ödev/KOZMOS-Amazon Yorumları için Duygu Analizi/vbo.PNG"))
+
+wc = WordCloud(background_color="black",
+               max_words=1000,
+               mask=tr_mask,
+               contour_width=3,
+               contour_color="firebrick")
+
+wc.generate(text)
+plt.figure(figsize=[10, 10])
+plt.imshow(wc, interpolation="bilinear")
+plt.axis("off")
+plt.show()
+
+wc.to_file("amazonvbowordcloud.png")
+
+
+##################################################
+# 3. Sentiment Analysis
+##################################################
+
+df["Review"].head()
+
+# nltk.download('vader_lexicon')
+
+sia = SentimentIntensityAnalyzer()
+
+df["Review"][0:10].apply(lambda x: sia.polarity_scores(x))
+
+df["Review"][0:10].apply(lambda x: sia.polarity_scores(x)["compound"])
+
+df["polarity_score"] = df["Review"].apply(lambda x: sia.polarity_scores(x)["compound"])
+
+###############################
+# 4. Feature Engineering
+###############################
+
+df.info()
+df.head()
+
+df["Review"][0:10].apply(lambda x: "pos" if sia.polarity_scores(x)["compound"] > 0 else "neg")
+
+df["sentiment_label"] = df["Review"].apply(lambda x: "pos" if sia.polarity_scores(x)["compound"] > 0 else "neg")
+
+df["sentiment_label"].value_counts()
+
+###############################
+# 4. Machine Learning
+###############################
+
+df["sentiment_label"] = LabelEncoder().fit_transform(df["sentiment_label"])
+
+y = df["sentiment_label"]
+X = df["Review"]
+
+###############################
+# TF-IDF
+###############################
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+tf_idf_word_vectorizer = TfidfVectorizer()
+X_tf_idf_word = tf_idf_word_vectorizer.fit_transform(X)
+
+###############################
+# 5. Sentiment Modeling
+###############################
+
+###############################
+# Logistic Regression
+###############################
+
+log_model = LogisticRegression().fit(X_tf_idf_word, y)
+
+cross_val_score(log_model,
+                X_tf_idf_word,
+                y,
+                scoring="accuracy",
+                cv=5).mean()
+# 0.8944925659084163
+
+random_review = pd.Series(df["Review"].sample(1).values)
+
+new_review = TfidfVectorizer().fit(X).transform(random_review)
+
+log_model.predict(new_review)
+
+
+###############################
+# Random Forests
+###############################
+
+# TF-IDF Word-Level
+rf_model = RandomForestClassifier().fit(X_tf_idf_word, y)
+cross_val_score(rf_model, X_tf_idf_word, y, cv=5, n_jobs=-1).mean()
+# 0.913742474242186
+
+# Logistic Regression cv hatası
+# 0.8944925659084163
+
+###############################
+# Hiperparametre Optimizasyonu
+###############################
+
+rf_model = RandomForestClassifier(random_state=17)
+
+rf_params = {"max_depth": [8, None],
+             "max_features": [7, "auto"],
+             "min_samples_split": [2, 5, 8],
+             "n_estimators": [100, 200]}
+
+rf_best_grid = GridSearchCV(rf_model,
+                            rf_params,
+                            cv=5,
+                            n_jobs=-1,
+                            verbose=1).fit(X_tf_idf_word, y)
+
+rf_best_grid.best_params_
+
+rf_final = rf_model.set_params(**rf_best_grid.best_params_, random_state=17).fit(X_tf_idf_word, y)
+
+cross_val_score(rf_final, X_tf_idf_word, y, cv=5, n_jobs=-1).mean()
+# 0.9183768966179526
